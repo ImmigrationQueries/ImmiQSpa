fd90ab982c0e5e6e4384db222cb137ab831a049f
AsifMeem <asif.foysalm@gmail.com>
2021-03-02 04:10:52 -0800

    Remove config from repo

--- src/services/firebaseAuth.ts
+++ src/services/firebaseAuth.ts
@@ -1,25 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-
-interface Config {
-    apiKey: string;
-    authDomain: string;
-    projectId: string;
-    storageBucket: string;
-    messagingSenderId: string;
-    appId: string;
-    measurementId: string;
-}
-
-const config: Config = {
-    apiKey: 'AIzaSyAdR84wXkgkCYY3U404ls8pT1YiuPin-A4',
-    authDomain: 'immiq-d58c9.firebaseapp.com',
-    projectId: 'immiq-d58c9',
-    storageBucket: 'immiq-d58c9.appspot.com',
-    messagingSenderId: '563957956214',
-    appId: '1:563957956214:web:e8a0c49a0b1bb9c9e8a957',
-    measurementId: 'G-Z50Q6RKS12',
-};
+import config from './firebaseConfig';
 
 const fb = firebase.initializeApp(config);
 
