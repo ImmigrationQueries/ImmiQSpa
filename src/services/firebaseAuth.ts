e0ac26963b3f978e66a64206b4a395531dbda0d6
AsifMeem <asif.foysalm@gmail.com>
2021-06-03 23:44:49 +1000

    Extract config to env vars

--- src/services/firebaseAuth.ts
+++ src/services/firebaseAuth.ts
@@ -1,8 +1,14 @@
 import 'firebase/auth';
 import firebase from 'firebase/app';
-import config from './firebaseConfig';
 
-const fb = firebase.initializeApp(config);
+const fb = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+});
 
 const fbAuth = fb.auth();
 
